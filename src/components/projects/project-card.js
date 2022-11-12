c27ab6d15f938edf6209d4e5a4fb30a8e86349cf
codeBiscuit <josh.rinehart.2k@gmail.com>
2022-11-12 11:25:44 -0500

    added flex wrap breakpoint for mobile on project cards

--- src/components/projects/project-card.js
+++ src/components/projects/project-card.js
@@ -1,8 +1,8 @@
 import React from "react";
 import './project-card.css'
 
-function ProjectCard({project}){
-    return(
+function ProjectCard({ project }) {
+    return (
         <div className="project-card card m-4 mb-5 p-3 border border-secondary">
             <div className="project-info d-flex align-items-center justify-content-around">
                 <label className="project-title m-3 p-1">{project.title}</label>
@@ -10,14 +10,14 @@ function ProjectCard({project}){
                     {project.demo && (
                         <a className="project-link icon-size px-1" href={project.demo} target="_blank">
                             <div className="link-button">
-                            <i className="fa-solid fa-globe"></i>
+                                <i className="fa-solid fa-globe"></i>
                             </div>
                         </a>
                     )}
                     {project.github && (
                         <a className="project-link icon-size px-1" href={project.github} target="_blank">
                             <div className="link-button">
-                            <i className="fa-brands fa-github p-3"></i>
+                                <i className="fa-brands fa-github p-3"></i>
                             </div>
                         </a>
                     )}
