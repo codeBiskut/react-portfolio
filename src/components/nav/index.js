621d77035f32a9e793a5e88c5f5cbc592c98641e
codeBiscuit <josh.rinehart.2k@gmail.com>
2022-11-12 11:15:41 -0500

    added hamburger menu functionality

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,17 +1,25 @@
 import React from "react";
 import "./nav.css"
 
-function Nav({handleClick, currentPage}) {
+function Nav({ handleClick, currentPage }) {
 
 
 
     return (
         <nav className="navbar navbar-expand-lg navbar-dark header-color">
             <a className="navbar-brand px-4" href="#">Joshua Rinehart</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+            <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
                 <div className="navbar-nav">
                     <a onClick={handleClick} className={`nav-item nav-link px-3 ${currentPage === 'About Me' ? 'active' : ''}`}>About Me</a>
                     <a onClick={handleClick} className={`nav-item nav-link px-3 ${currentPage === 'Projects' ? 'active' : ''}`}>Projects</a>
